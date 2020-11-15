014d5a059f4d48299f0ac4a9bb812f6cdcccd482
keshav220 <59433700+keshavg220@users.noreply.github.com>
2020-11-15 21:14:40 +0530

    first commit

---
+++ routes.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const Joi = require('@hapi/joi')
+const { db, getItems, updateQuantity } = require('./db')
+
+const router = express.Router()
+
+const itemSchema = Joi.object().keys({
+  name: Joi.string(),
+  quantity: Joi.number().integer().min(0)
+})
+
+router.post('/item', (req, res) => {
+  const item = req.body
+  console.log(req.body)
+  const result = itemSchema.validate(item)
+  if (result.error) {
+    console.log(result.error)
+    res.status(400).end()
+    return
+  }
+  const collection = db.collection('items')
+  collection.insertOne(item).then(() => {
+      res.status(200).end()
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).end()
+    })
+})
+
+router.get('/items', (req, res) => {
+   const collection = db.collection('items')
+   collection.find({}).toArray().then((items) => {
+      items = items.map((item) => ({
+        id: item._id,
+        name: item.name,
+        quantity: item.quantity
+      }))
+      res.json(items)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).end()
+    })
+})
+
+router.put('/item/:id/quantity/:quantity', (req, res) => {
+  const { id, quantity } = req.params
+  const collection = db.collection('items')
+  collection.updateOne({ _id: ObjectId(id) }, { $inc: { parseInt(quantity) } })
+    .then(() => {
+      res.status(200).end()
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).end()
+    })
+})
+
+module.exports = router
