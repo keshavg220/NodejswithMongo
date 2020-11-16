56314604cfac357a11730879c4ccc3201c7aeff5
yogeshhelpful <007saurabhgoyal@gmail.com>
2020-11-17 02:31:02 +0530

    change

--- public/js/my-login.js
+++ public/js/my-login.js
@@ -1,17 +1,24 @@
 $(function() {
 });
+function onFailure(error) {
+  console.log(error);
+}
+
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
+function renderButton() {
+  gapi.signin2.render('my-signin2', {
+    'scope': 'profile email',
+    'width': 240,
+    'height': 50,
+    'longtitle': true,
+    'theme': 'dark',
+    'onsuccess': onSignIn,
+    'onfailure': onFailure
+  });
+}
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
- }
