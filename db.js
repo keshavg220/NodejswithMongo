067834d023174c90bede3ff15037af122be1ce32
yogeshhelpful <007saurabhgoyal@gmail.com>
2020-11-18 23:26:02 +0530

    change

--- db.js
+++ db.js
@@ -8,7 +8,6 @@ const dbName = 'Items'
 const init = () =>
   MongoClient.connect(connectionUrl, { useNewUrlParser: true }).then((client) => {
     db = client.db(dbName)
-    console.log("db connected ")
   })
 
 module.exports = { init}
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const http = require('http');
 var db ;
 const { init } = require('./db')
 const routes = require('./routes')
@@ -7,7 +8,14 @@ const app = express()
 app.use(bodyParser.json())
 app.use(express.static(__dirname + "/public"));
 app.use(routes)
+const server = http.createServer(app);
+const port = process.env.PORT || 3000
 init().then(() => {
-  console.log('starting server on port 3000')
-  app.listen(3000)
+console.log("database connected") 
 })
+
+ server.listen(port, function() {
+  console.log('Express server running on *:' + port);
+});
+
+
